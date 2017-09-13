c0916d8289db11635efc88dfa7292364db85a665
Oscar Silva <oscar.silva@mdp.com.pe>
2017-09-13 12:51:41 -0500

    Muestra la lista de miembros de la organización

---
+++ assets/js/scouttech.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $.ajax("https://api.github.com/orgs/ScoutTech/members")
+        .done(function (data) {
+                data.forEach(function(x) {
+                    $("#member").append(
+                        $("<li>").append(
+                            $("<a>", {
+                                href: x.html_url,
+                                target: "_blank"
+                            }).prepend(
+                                $("<img>", {
+                                    src: x.avatar_url
+                                })
+                            ).append(
+                                $("<span>", {
+                                    text: x.login
+                                })
+                            )
+                        )
+                    );
+                });
+        });
+});
