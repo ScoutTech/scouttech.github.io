04b168f2292935c7521a7754213f18699fadd341
ogsilvaa <ogsilvaa@gmail.com>
2017-09-14 01:50:44 -0500

    creación de página de issues y modfiicacion de menu y cabeceras

--- assets/js/scouttech.js
+++ assets/js/scouttech.js
@@ -1,7 +1,14 @@
 ﻿var APIS = {
     Miembros: "https://api.github.com/orgs/ScoutTech/members",
-    Repositorios:"https://api.github.com/orgs/ScoutTech/repos"
+    Repositorios: "https://api.github.com/orgs/ScoutTech/repos"
 };
+
+var MENU = [
+    { href: "index.html", text: "Principal" },
+    { href: "https://scouttech.github.io/seeonee", text: "Seeonee" }
+];
+
+var HEADER = { nombre: "Scout Tech", sufijo: " software", link: "index.html" };
 
 $(document).ready(function () {
     $.ajax(APIS.Miembros)
@@ -30,9 +37,16 @@ $(document).ready(function () {
             data.forEach(function (x) {
                 $("#repos").append(
                     $("<li>").append(
+                            $("<a>", {
+                                class: "fa fa-caret-right fa-5x",
+                                href: "issues.html?repo=" + x.name,
+                                alt: "Ver Issues"
+                            })
+                        ).append(
                         $("<a>", {
                             href: x.html_url,
-                            target:"_blank"
+                            target: "_blank",
+                            alt:"Ver Repositorio"
                         }).append(
                             $("<h3>", {
                                 text: x.name,
@@ -54,6 +68,22 @@ $(document).ready(function () {
                         )
                     )
                 );
+            });
         });
-});
+    MENU.forEach(function (x) {
+        $("#menu").find(".links").append(
+            $("<li>").append(
+                $("<a>", {
+                    href: x.href,
+                    text: x.text
+                })
+            )
+        );
+    });
+    $("#header").find(".logo").append(
+        $("<a>", { href: HEADER.link, text: HEADER.nombre })
+        .append($("<span>", { text: HEADER.sufijo })
+        )
+    );
+    $("head").find("title").text(HEADER.nombre + HEADER.sufijo);
 });
